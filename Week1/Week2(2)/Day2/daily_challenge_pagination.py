c0e1eef1dee0bc3d709da3e1647c87007c8f758b
SydneyH613 <sydneyhoffman27@gmail.com>
2026-07-06 18:10:31 +0300

    commit

---
+++ Week1/Week2(2)/Day2/daily_challenge_pagination.py
@@ -0,0 +1,74 @@
+import math
+
+
+class Pagination:
+    def __init__(self, items=None, page_size=10):
+        self.items = items if items is not None else []
+        self.page_size = page_size
+        self.current_idx = 0
+        self.total_pages = math.ceil(len(self.items) / self.page_size)
+
+    def get_visible_items(self):
+        start = self.current_idx * self.page_size
+        end = start + self.page_size
+        return self.items[start:end]
+
+    def go_to_page(self, page_num):
+        if page_num < 1 or page_num > self.total_pages:
+            raise ValueError(f'Page number must be between 1 and {self.total_pages}')
+        self.current_idx = page_num - 1
+        return self
+
+    def first_page(self):
+        self.current_idx = 0
+        return self
+
+    def last_page(self):
+        self.current_idx = self.total_pages - 1
+        return self
+
+    def next_page(self):
+        if self.current_idx < self.total_pages - 1:
+            self.current_idx += 1
+        return self
+
+    def previous_page(self):
+        if self.current_idx > 0:
+            self.current_idx -= 1
+        return self
+
+    def __str__(self):
+        return "\n".join(str(item) for item in self.get_visible_items())
+
+
+if __name__ == "__main__":
+    alphabetList = list("abcdefghijklmnopqrstuvwxyz")
+    p = Pagination(alphabetList, 4)
+
+    print(p.get_visible_items())
+    # ['a', 'b', 'c', 'd']
+
+    p.next_page()
+    print(p.get_visible_items())
+    # ['e', 'f', 'g', 'h']
+
+    p.last_page()
+    print(p.get_visible_items())
+    # ['y', 'z']
+
+    try:
+        p.go_to_page(10)
+        print(p.current_idx + 1)
+    except ValueError as e:
+        print(f'ValueError: {e}')
+
+    try:
+        p.go_to_page(0)
+    except ValueError as e:
+        print(f'ValueError: {e}')
+
+    print(str(p))
+
+    p.first_page()
+    print(p.next_page().next_page().next_page().get_visible_items())
+    # ['m', 'n', 'o', 'p']
