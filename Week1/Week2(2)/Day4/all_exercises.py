f18850e99735d1fc285da075b1fdb4c516e4b79f
SydneyH613 <sydneyhoffman27@gmail.com>
2026-07-06 18:21:27 +0300

    m

--- Week1/Week2(2)/Day4/all_exercises.py
+++ Week1/Week2(2)/Day4/all_exercises.py
@@ -1,6 +1,10 @@
 import random
+import json
 
 
+# ---------------------------------------------------------------------------
+# Exercise 1: Random Sentence Generator
+# ---------------------------------------------------------------------------
 def get_words_from_file(file_path):
     with open(file_path, "r") as f:
         content = f.read()
@@ -32,5 +36,31 @@ def main():
     print(get_random_sentence(length))
 
 
-if __name__ == "__main__":
-    main()
+main()
+
+
+# ---------------------------------------------------------------------------
+# Exercise 2: Working with JSON
+# ---------------------------------------------------------------------------
+sampleJson = """{
+   "company":{
+      "employee":{
+         "name":"emma",
+         "payable":{
+            "salary":7000,
+            "bonus":800
+         }
+      }
+   }
+}"""
+
+data = json.loads(sampleJson)
+
+salary = data["company"]["employee"]["payable"]["salary"]
+print(salary)
+# 7000
+
+data["company"]["employee"]["birth_date"] = "1998-05-14"
+
+with open("modified_data.json", "w") as f:
+    json.dump(data, f, indent=4)
